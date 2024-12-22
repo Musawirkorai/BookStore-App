547573e9dedb7c6356511ba0582538f796c8f230
Musawirkorai <musawirkorai@gmail.com>
2024-12-23 00:11:58 +0800

    Responisve Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-
+import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-    <nav>Navbar</nav>
-     <main>
+    <Navbar/>
+     <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
       <Outlet/>
      </main>
      <footer>Footer</footer>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+	<header className="max-w-screen-2xl mx-auto px-4 py-4">
+
+		<nav className="flex justify-between items-center">
+
+			<div>Logo & Search Box</div>
+
+
+
+		</nav>
+
+	
+	</header>
+  )
+}
+
+export default Navbar
